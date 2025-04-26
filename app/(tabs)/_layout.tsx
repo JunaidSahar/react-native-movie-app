5a5fb9c5784339bd3cbee6962dbc27cffd2bf96c
junaidsahar <imaadahmed300@gmail.com>
2025-04-26 02:21:23 -0700

    route folders

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Tabs } from "expo-router"; 
+
+const _Layout = () => {
+    return (
+        <Tabs>
+            <Tabs.Screen name="index" options={{ title: "Home", headerShown: false }} />
+        </Tabs>
+
+    )
+}
